9573efac8c5e76305c7e8ceeb5135a628cf52317
Lucas <tsaoyuan@gmail.com>
2023-03-23 15:25:23 +0800

    feat: confirmed and User::create
    - 用 `postman` call api (重複註冊同一 user), 會被導向至首頁, postman 無法顯示 json 格式的錯誤訊息
      - 處理方式: postman 的 Headers 內，加上 key = Accept 和 value = application/json

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,24 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
     public function register(Request $request){
         // dd($request);
-        // 方法一: $this->validate($request, [...略
         $validated = $this->validate($request, [
             'name' => 'required|min:3|max:64',
-            'email' => 'required|email|max:255|unique:users,email',
-            'password' => 'required|min:4|max:8'
+            'email' => 'required|email|max:255|unique:users,email', 
+            'password' => 'required|min:4|max:8|confirmed'// password_confirmation
         ]);
 
-        // 方法二: $request->validate([...略
-        // $validated = $request->validate([
-        //     'name' => 'required|min:3|max:64',
-        //     'email' => 'required|email|max:255|unique:users,email',
-        //     'password' => 'required|min:4|max:8'
-        // ]);
-        dd($validated);
+        // dd($validated);
+        User::create($validated);
     }
 }
