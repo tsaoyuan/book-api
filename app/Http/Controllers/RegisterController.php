92af7e02a725fed878576aa37e076b06104237bd
Lucas <tsaoyuan@gmail.com>
2023-03-24 11:07:46 +0800

    feat: validator and 自定義的差異
    - validator 意指 email 加上 unique, 錯誤訊息就會是以 `lang/en/validation.php` 的 unique 訊息
    - abort_if 自訂訊息 (與前端溝通遇到 4xx錯誤, 前端依照 message 發送訊息)
      - 溝通後, 前端從 status code 給訊息, 前端無須再追查 response 內的 json 內容 (detail)

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,14 +11,17 @@ class RegisterController extends Controller
 {
     public function register(Request $request){
         // dd($request);
+
+        // validator
         $validated = $this->validate($request, [
             'name' => 'required|min:3|max:64',
-            'email' => 'required|email|max:255|unique:users,email', 
+            'email' => 'required|email|max:255', 
             'password' => 'required|min:4|max:8|confirmed'// password_confirmation
         ]);
 
         // dd($validated);
         // 將 call api 遇到 email 已重複(已註冊過) 的 json 格式顯示訊息, 用 abort_if() 客制成有效的訊息  
+        // 自定義
         abort_if(
             User::where('email', $request->input('email'))->first(),
             Response::HTTP_BAD_REQUEST,
