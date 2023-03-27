47722782870b43316e40e9aeba7d7ed4a8a10008
Lucas <tsaoyuan@gmail.com>
2023-03-27 11:13:22 +0800

    feat: add role to users table
    - `Models/User.php` 設定角色權限 對應 數字，`ROLE_ADMIN = 1`, `ROLE_NORMAL = 2`, 未來若有其他角色，一一填上
      - 角色權限使用文字 或數字表示皆可，端看設計者決定，無好壞之分
    - role 設定 UNSIGNED INTEGER 預設 2(User::ROLE_NORMAL)
    註. Migrations 時，記得搭配`up` 設計相對應的 `down()` 方法，使用 roll back(`php artisan migrate:rollback`) 恢復 databases 設定，避免直接操作 database

---
+++ database/migrations/2023_03_27_024805_add_role_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // creates an UNSIGNED INTEGER equivalent column
+            $table->unsignedInteger('role')->default(User::ROLE_NORMAL)->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('role');
+        });
+    }
+};
